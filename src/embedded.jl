ee3839a730fe433e35c91cc171702b5736e67145
Guy Van den Broeck <guyvdb@gmail.com>
2021-11-08 20:27:06 -0800

    add @dice macro hygiene

--- src/embedded.jl
+++ src/embedded.jl
@@ -5,22 +5,23 @@ export @dice
 # extentions to Dice.jl to support embedding dice-like code in Julia
 
 ite(cond::ProbBool, x, y) = 
-    ite(cond, convert(ProbData,x), convert(ProbData,y))
+    ite(cond, val2dist(x, cond.mgr), 
+              val2dist(y, cond.mgr))
 
 Base.:&(x::ProbBool, y::Bool) = 
-    x & convert(ProbBool,y)
+    x & val2dist(y, x.mgr)
 
 Base.:&(x::Bool, y::ProbBool) = 
     y & x
 
 Base.:|(x::ProbBool, y::Bool) = 
-    x | convert(ProbBool,y)
+    x | val2dist(y, x.mgr)
 
 Base.:|(x::Bool, y::ProbBool) = 
     y | x
 
-Base.convert(::Type{ProbData}, b::Bool) = 
-    Base.convert(ProbBool, b)
+val2dist(x::Bool, mgr) = 
+    ProbBool(mgr, x)
 
 """
 macro to process dice code before running it
@@ -28,9 +29,11 @@ currently it makes if-then-else, &&, and || polymorphic
 """
 macro dice(code)
 
-    # TODO figure out how to use `esc` to correctly bind methods defined outside of the macro
+    # manual hygiene
+    mgr = gensym(:mgr)
+    flip = gensym(:flip)
 
-    transformed_code = postwalk(code) do x
+    transformed_code = postwalk(esc(code)) do x
         # TODO: replace by custom desugaring that is still lazy for boolean guards
         # this will not work in general
         # for example when control flow has `return`` inside
@@ -38,6 +41,7 @@ macro dice(code)
             @assert length(x.args) == 3 "@dice macro only supports purely functional if-then-else"
             return Expr(:call, :ifelse, x.args...)
         end
+        @capture(x, flip(P_)) && return :($flip($P)) 
         @capture(x, A_ || B_) && return :($A | $B) 
         @capture(x, A_ && B_) && return :($A & $B) 
         return x
@@ -45,15 +49,12 @@ macro dice(code)
 
     return quote
         
-        # prelims tied to one manage
-        mgr = Dice.default_manager()
+        $(esc(mgr)) = default_manager()
         
-        Dice.flip(prob::Number) = 
-            Dice.flip(mgr) #ignore prob for now
-        
-        Base.convert(::Type{ProbBool}, b::Bool) = 
-            Dice.ProbBool(mgr, b)
-            
+        function $(esc(flip))(prob::Number) 
+            Dice.flip($(esc(mgr))) #ignore prob for now
+        end
+
         # transformed user code
         $transformed_code
     end
---
+++ test/grah_reachability.jl
@@ -0,0 +1,36 @@
+using Pkg; Pkg.activate(@__DIR__);
+
+using Dice: @dice, num_flips, num_nodes
+
+function reachable(adjacency::Matrix, src::Int, dest::Int)
+    n = size(adjacency, 1)
+    reachable = Vector(undef, n)
+    reachable .= false
+    reachable[src] = true
+    for i=1:n
+        for node=1:n
+            reachable[node] |= reduce(|, reachable .& adjacency[:, node])
+        end
+    end
+    reachable[dest]
+end
+
+@dice begin
+
+    n = 7
+
+    # run on sampled deterministic graph
+    adjacency_sampled = rand(Bool, n, n)
+    r = reachable(adjacency_sampled, 1, n)
+    println("Sampled graph reachability: ", r)
+
+    # run on random graph
+    adjacency_rand = [flip(0.5) for i=1:n, j=1:n]
+    r = reachable(adjacency_rand, 1, n)
+
+    @assert num_flips(r) == n*n-3n+3
+    println("Number of flips used: ", num_flips(r))
+    println("Number of BDD nodes: ", num_nodes(r))
+
+end
+
