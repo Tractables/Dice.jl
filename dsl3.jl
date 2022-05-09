8e98a9000c9f274e30f35a9d144b5a23cdda522b
Guy Van den Broeck <guyvdb@gmail.com>
2022-05-09 15:24:42 -0700

    figuring out variable scope

--- dsl3.jl
+++ dsl3.jl
@@ -2,32 +2,58 @@ using IRTools
 
 ##################
 
-function foo(x)
+function foo(x,y)
+    z = y+0.05
     if x
-        0.4
+        0.4 + z
     else
-        0.1
+        0.1 + z
     end
 end
 
 ifelse(g::AbstractFloat, t, e) = g*t + (1-g)*e
 
-foo(true)
-foo(0.9)
+foo(true,0.1)
+foo(0.9,0.1)
 
-IRTools.IR(typeof(foo), Any) 
+IRTools.IR(typeof(foo), Any, Any) 
 
 ##################
 
-using IRTools: blocks, block!, canbranch, IR, argument!, return!, xcall, func, branches, isconditional, Branch
+using IRTools: blocks, block!, canbranch, IR, argument!, return!, xcall, func, isconditional, Branch
 
 function transform(ir)
     # point each conditional `br`` to its polymorphism block
+    @show ir
     for i in eachindex(blocks(ir))
         block = IRTools.block(ir,i)
         polybrs = Branch[]
-        for br in branches(block) 
+        branches = IRTools.branches(block) 
+
+        # which variables are relevant to the remainder of the computation?
+        args = [Set{IRTools.Variable}() for _ in 1:length(branches)]
+
+        @show i
+        for j in length(branches):-1:1
+            br = branches[j]    
+            if isconditional(br) && br.condition isa IRTools.Variable
+                push!(args[j], br.condition)
+            end
+            for x in br.args 
+                if x  isa IRTools.Variable
+                    push!(args[j],x)
+                end
+            end
+            if j < length(branches)
+                union!(args[j], args[j+1])
+            end
+            @show args
+        end
+
+        for j in 1:length(branches)
+            br = branches[j]            
             !isconditional(br) && continue
+
 
             # add a polymorphism block to escape to when guard is non-boolean
             poly = block!(ir)
@@ -45,7 +71,7 @@ function transform(ir)
             polybr = Branch(isbool, length(blocks(ir)), [cond])
             push!(polybrs, polybr)
         end
-        prepend!(branches(block), polybrs) 
+        prepend!(branches, polybrs) 
     end
     ir
 end
