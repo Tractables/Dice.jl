09e9d3a6ae11b47b124df70311b6f33f373ebdb4
PoorvaGarg <poorva98@gmail.com>
2022-05-20 10:17:56 -0700

    fixed the test

--- test/fixedpoint_test.jl
+++ test/fixedpoint_test.jl
@@ -8,7 +8,7 @@ using Dice: CuddMgr, DistBool
     @test infer((@dice begin
                         a = DistFixParam{2, 1}([flip(0.5), flip(0.5)])
                         b = DistFixParam{2, 1}([flip(0.5), flip(0.5)])
-                        prob_equals((a/b)[1], 0.5) 
+                        prob_equals((a/b)[1], 1) 
                        end), :bdd) ≈ 0.25
     @test infer((@dice DistBool(dicecontext(), 0.5)), :bdd) ≈ 0.5
 
