a1f3c9eb27c38f413caf0278c0b8082f3f4dc75f
Ryan Tjoa <tjoa@g.ucla.edu>
2022-06-29 22:03:40 -0700

    Add save_cg(): Dist to straightline code

--- src/Dice.jl
+++ src/Dice.jl
@@ -12,6 +12,7 @@ include("lib/vector.jl")
 include("lib/tree.jl")
 include("lib/sample.jl")
 include("lib/ocaml_interface.jl")
+include("lib/cg_to_code.jl")
 # include("lib/tuple.jl")
 
 include("inference/infer.jl")
