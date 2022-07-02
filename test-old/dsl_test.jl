227a1881b56bbc042202154098442c4b67c7c3ec
Guy Van den Broeck <guyvdb@gmail.com>
2022-07-01 20:48:09 -0700

    test cleanup

--- test-old/dsl_test.jl
+++ test-old/dsl_test.jl
@@ -5,8 +5,6 @@ using Dice: CuddMgr, DistBool
 
 @testset "Sugar Tests" begin
     
-    @test infer_bool(@dice_ite flip(0.5)) ≈ 0.5
-    @test infer_bool((@dice_ite DistBool(0.5))) ≈ 0.5
 
     @test infer_bool((@dice_ite prob_equals(DistInt(42), 42))) ≈ 1
 
