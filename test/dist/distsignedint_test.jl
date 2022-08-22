9db7e961d71947402808dd38ea9f14eb439068a2
PoorvaGarg <poorva98@gmail.com>
2022-08-22 09:51:37 -0700

    removed commented code

--- test/dist/distsignedint_test.jl
+++ test/dist/distsignedint_test.jl
@@ -39,62 +39,6 @@ using Dice: Flip, ifelse, num_ir_nodes
     @test prob_equals(y, DistSignedInt{4}(3))
 end
 
-# @testset "DistSignedInt operations" begin
-    
-#     x = DistInt{4}([true, false, true, false]) # 10
-#     y = DistInt{4}([false, false, true, true]) # 3
-#     p = pr(x + y)
-#     @test p[12] ≈ 0
-#     @test p[13] ≈ 1
-#     @test p[14] ≈ 0
-#     p = pr(x - y)
-#     @test p[6] ≈ 0
-#     @test p[7] ≈ 1
-#     @test p[8] ≈ 0
-
-#     z = uniform(DistInt{4}, 3)
-#     y2 = DistInt{4}([false, false, true, false])
-#     t = z + y
-#     p = pr(t)
-#     @test issetequal(keys(p), 3 .+ (0:(2^3-1)))
-#     @test all(values(p) .≈ 1/2^3)
-#     p = pr((@dice t - y2), ignore_errors=true)
-#     @test issetequal(keys(p), 1 .+ (0:(2^3-1)))
-#     @test all(values(p) .≈ 1/2^3)
-
-#     w = uniform(DistInt{4}, 3)
-#     p = pr(z + w)
-#     n = 2^3
-#     for i=0:(2^4-1)
-#         @test p[i] ≈ (n - abs(i-(n-1)))/n^2
-#     end
-
-#     w = uniform(DistInt{4}, 2)
-#     z = uniform(DistInt{4}, 2)
-#     p = pr((@dice w + y - z), ignore_errors=true)
-#     n = 2^2
-#     for i=0:6
-#         @test p[i] ≈ (n - abs(i-(n-1)))/n^2
-#     end
-
-#     @test_throws Exception pr(uniform(DistInt{3}, 3)+uniform(DistInt{3}, 3))
-#     @test_throws Exception pr(@dice uniform(DistInt{3}, 3) + uniform(DistInt{3}, 3))
-
-#     @test_throws Exception pr(uniform(DistInt{3}, 3)-uniform(DistInt{3}, 3))
-#     @test_throws Exception pr(@dice uniform(DistInt{3}, 3) - uniform(DistInt{3}, 3))
-    
-#     x = DistInt{3}([false, flip(0.5), flip(0.5)]) # uniform(0, 4)
-#     y = DistInt{3}([false, flip(0.5), flip(0.5)])
-#     p = pr(prob_equals(x, y))
-#     @test p[1] ≈ 4/16
-
-#     x = DistInt{3}([false, flip(0.5), flip(0.5)]) # uniform(0, 4)
-#     y = DistInt{3}([false, flip(0.5), flip(0.5)])
-#     p = pr(x < y)
-#     @test p[1] ≈ 6/16
-
-# end
-
 @testset "DistSignedInt casting" begin
     y = DistSignedInt{4}([flip(0.5), false, true, true])
     z = convert(y, DistSignedInt{5})
@@ -117,39 +61,3 @@ end
     @test expectation(y) ≈ mean
 
 end
-
-# @testset "DistInt uniform" begin
-#     uniform_funcs = [uniform_arith, uniform_ite]
-
-#     map(uniform_funcs) do uniform 
-#         x = uniform(DistInt{3}, 0, 7)
-#         p = pr(x)
-#         for i=0:6
-#             @test p[i] ≈ 1/7
-#         end
-#         @test p[7] ≈ 0
-        
-#         @test_throws Exception uniform(DistInt{3}, 0, 10)
-#         @test_throws Exception uniform(DistInt{3}, -1, 7)
-#         @test_throws Exception uniform(DistInt{3}, 4, 3)
-#         @test_throws Exception uniform(DistInt{3}, 3, 3)
-
-#         x = uniform(DistInt{3}, 3, 4)
-#         p = pr(x)
-#         @test p[3] ≈ 1
-
-#         x = uniform(DistInt{5}, 3, 17)
-#         p = pr(x)
-#         for i=3:16
-#             @test p[i] ≈ 1/14
-#         end
-#         y = DistInt{5}(10)
-#         p = pr(x < y)
-#         @test p[1] ≈ 7/14
-
-#         z = DistInt{5}(0)
-#         p = pr(prob_equals(x, z))
-#         @test p[1] ≈ 0
-
-#     end
-# end
