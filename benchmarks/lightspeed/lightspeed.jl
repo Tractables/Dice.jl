f9771ad95e6ea1d29dd40f6049ac1a1eaa1c004e
PoorvaGarg <poorva98@gmail.com>
2022-10-03 21:22:49 -0700

    normal_mixture

--- benchmarks/lightspeed/lightspeed.jl
+++ benchmarks/lightspeed/lightspeed.jl
@@ -1,22 +1,6 @@
 using Pkg; Pkg.activate("$(@__DIR__)/../")
 using Dice, Distributions
 
-#=
-data {
-  int<lower=0> N;
-  vector[N] y;
-  vector[N] y_lag;
-}
-parameters {
-  vector[2] beta;
-  real<lower=0> sigma;
-}
-model {
-  beta ~ normal(1,1);
-  y ~ normal(beta[1] + beta[2] * y_lag, sigma);
-}
-=#
-
 precision = 1
 DFiP = DistFixedPoint{8+precision, precision}
 num_pieces = 16
