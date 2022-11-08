d12308a5827dc757b8473172efdc566e51866fce
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2022-11-08 13:44:16 -0800

    use Test package for char/string tests

--- test/dist/char_test.jl
+++ test/dist/char_test.jl
@@ -1,37 +1,40 @@
+using Test
 using Dice
 
-c = @dice_ite begin
-    if flip(1/10)
-        DistChar('a')
-    elseif flip(2/9)
-        DistChar('D')
-    elseif flip(3/7)
-        DistChar(' ')
-    else
-        DistChar('!')
+@testset "DistChar core" begin
+    c = @dice_ite begin
+        if flip(1/10)
+            DistChar('a')
+        elseif flip(2/9)
+            DistChar('D')
+        elseif flip(3/7)
+            DistChar(' ')
+        else
+            DistChar('!')
+        end
     end
-end
 
-dist = pr(c)
+    dist = pr(c)
 
-@assert sum(values(dist)) ≈ 1
-@assert dist['a'] ≈ 1/10
-@assert dist['D'] ≈ 2/10
-@assert dist[' '] ≈ 3/10
-@assert dist['!'] ≈ 4/10
+    @test sum(values(dist)) ≈ 1
+    @test dist['a'] ≈ 1/10
+    @test dist['D'] ≈ 2/10
+    @test dist[' '] ≈ 3/10
+    @test dist['!'] ≈ 4/10
 
-c = @dice_ite begin
-    if flip(1/10)
-        DistChar('a')
-    elseif flip(2/9)
-        DistChar('b')
-    elseif flip(3/7)
-        DistChar('c')
-    else
-        DistChar('b')
+    c = @dice_ite begin
+        if flip(1/10)
+            DistChar('a')
+        elseif flip(2/9)
+            DistChar('b')
+        elseif flip(3/7)
+            DistChar('c')
+        else
+            DistChar('b')
+        end
     end
+    @test pr(c < DistChar('b'))[true] ≈ 1/10
+    @test pr(c <= DistChar('b'))[true] ≈ 7/10
+    @test pr(c >= DistChar('b'))[true] ≈ 9/10
+    @test pr(c > DistChar('b'))[true] ≈ 3/10
 end
-@assert pr(c < DistChar('b'))[true] ≈ 1/10
-@assert pr(c <= DistChar('b'))[true] ≈ 7/10
-@assert pr(c >= DistChar('b'))[true] ≈ 9/10
-@assert pr(c > DistChar('b'))[true] ≈ 3/10
--- test/dist/string_test.jl
+++ test/dist/string_test.jl
@@ -1,71 +1,84 @@
+using Test
 using Dice
 
-# Test concatenation, appending, ifelse
-cg = @dice begin
-    s = if flip(3/5) DistString("sand") else DistString("san") end
-    s = if flip(2/3) s + 'd' else s end
-    t = if flip(1/10) DistString("wich") else DistString("box") end
-    s + t
+@testset "DistString core" begin
+    cg = @dice begin
+        s = if flip(3/5) DistString("sand") else DistString("san") end
+        if flip(2/3) s + 'd' else s end
+    end
+    dist = pr(cg)
+    @test dist["sand"] ≈ 3/5 * 1/3 + 2/5 * 2/3
+    @test dist["sandd"] ≈ 3/5 * 2/3
+    @test dist["san"] ≈ 2/5 * 1/3
+
+    # Test concatenation, appending, ifelse
+    cg = @dice begin
+        s = if flip(3/5) DistString("sand") else DistString("san") end
+        s = if flip(2/3) s + 'd' else s end
+        t = if flip(1/10) DistString("wich") else DistString("box") end
+        s + t
+    end
+
+    dist = pr(cg)
+    @test sum(values(dist)) ≈ 1
+    @test dist["sandwich"] ≈ 7/150
+    @test dist["sandbox"] ≈ 21/50
+    @test dist["sanwich"] ≈ 1/75
+    @test dist["sanbox"] ≈ 3/25
+    @test dist["sanddwich"] ≈ 1/25
+    @test dist["sanddbox"] ≈ 9/25
+
+    # Test concatenation for empty strings
+    cg = DistString("") + DistString("")
+    dist = pr(cg)
+    @test sum(values(dist)) ≈ 1
+    @test dist[""] ≈ 1
+
+
+    # Test getindex, setindex
+    cg = @dice begin
+        s = if flip(0.6) DistString("abc") else DistString("xyz") end
+
+        # Choose whether to change index 1 (Pr=0.3) or 2 (Pr = 0.7)
+        i = if flip(0.3) DistUInt32(1) else DistUInt32(2) end
+
+        c = if flip(0.1) DistChar('d') else DistChar('e') end
+        s = prob_setindex(s, i, c)
+        prob_equals(DistString("aec"), s)
+    end
+    @test pr(cg)[true] ≈ 0.6*0.7*0.9
+
+    # Test lessthan
+    cg = @dice begin
+        s = if flip(0.6) DistString("abc") else DistString("xyz") end
+        t = if flip(0.6) DistString("abc") else DistString("xyz") end
+        s < t
+    end
+    @test pr(cg)[true] ≈ 0.6 * 0.4
+
+
+    # Test lessthan for identical strings
+    cg = @dice begin
+        DistString("abc") < DistString("abc")
+    end
+    @test pr(cg)[true] ≈ 0
+
+
+    # Test lessthan for strings that differ only in length
+    cg = @dice begin
+        DistString("abc") < DistString("abca")
+    end
+    @test pr(cg)[true] ≈ 1
+    cg = @dice begin
+        DistString("abca") < DistString("abc")
+    end
+    @test pr(cg)[true] ≈ 0
+
+    # Test leq, geq
+    @test pr(DistString("abc") <= DistString("abc"))[true] == 1
+    @test pr(DistString("abc") >= DistString("abc"))[true] == 1
+    @test pr(DistString("abcd") <= DistString("abc"))[true] == 0
+    @test pr("abc" <= DistString("abc"))[true] == 1
+    @test pr(DistString("abc") >= "abc")[true] == 1
+    @test pr("abcd" <= DistString("abc"))[true] == 0
 end
-dist = pr(cg)
-@assert sum(values(dist)) ≈ 1
-@assert dist["sandwich"] ≈ 7/150
-@assert dist["sandbox"] ≈ 21/50
-@assert dist["sanwich"] ≈ 1/75
-@assert dist["sanbox"] ≈ 3/25
-@assert dist["sanddwich"] ≈ 1/25
-@assert dist["sanddbox"] ≈ 9/25
-
-# Test concatenation for empty strings
-cg = DistString("") + DistString("")
-dist = pr(cg)
-@assert sum(values(dist)) ≈ 1
-@assert dist[""] ≈ 1
-
-
-# Test getindex, setindex
-cg = @dice_ite begin
-    s = if flip(0.6) DistString("abc") else DistString("xyz") end
-
-    # Choose whether to change index 1 (Pr=0.3) or 2 (Pr = 0.7)
-    i = if flip(0.3) DistUInt32(1) else DistUInt32(2) end
-
-    c = if flip(0.1) DistChar('d') else DistChar('e') end
-    s = prob_setindex(s, i, c)
-    prob_equals(DistString("aec"), s)
-end
-@assert pr(cg)[true] ≈ 0.6*0.7*0.9
-
-# Test lessthan
-cg = @dice begin
-    s = if flip(0.6) DistString("abc") else DistString("xyz") end
-    t = if flip(0.6) DistString("abc") else DistString("xyz") end
-    s < t
-end
-@assert pr(cg)[true] ≈ 0.6 * 0.4
-
-
-# Test lessthan for identical strings
-cg = @dice begin
-    DistString("abc") < DistString("abc")
-end
-@assert pr(cg)[true] ≈ 0
-
-
-# Test lessthan for strings that differ only in length
-cg = @dice begin
-    DistString("abc") < DistString("abca")
-end
-@assert pr(cg)[true] ≈ 1
-cg = @dice begin
-    DistString("abca") < DistString("abc")
-end
-@assert pr(cg)[true] ≈ 0
-
-# Test leq, geq
-@assert pr(DistString("abc") <= DistString("abc"))[true] == 1
-@assert pr(DistString("abc") >= DistString("abc"))[true] == 1
-@assert pr(DistString("abcd") <= DistString("abc"))[true] == 0
-@assert pr("abc" <= DistString("abc"))[true] == 1
-@assert pr(DistString("abc") >= "abc")[true] == 1
-@assert pr("abcd" <= DistString("abc"))[true] == 0
