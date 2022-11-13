db6b69bb4ddadaf9eeb7e4cbec01ba0a742e54f2
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2022-11-12 19:47:40 -0800

    add probabilistic tuple ite

--- src/dist/tuple.jl
+++ src/dist/tuple.jl
@@ -13,3 +13,11 @@ tobits(x::Matrix) =
 
 frombits(x::Matrix, world) = 
     map(v -> frombits(v, world), x)
+
+tobits(x::Matrix) = 
+    mapreduce(tobits, vcat, x)
+
+frombits(x::Matrix, world) = 
+    map(v -> frombits(v, world), x)
+Base.ifelse(cond::Dist{Bool}, then::Tuple, elze::Tuple) =
+    Tuple(ifelse(cond, x, y) for (x, y) in zip(then,elze))
