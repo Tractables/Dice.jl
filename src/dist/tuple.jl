78800c9ab4b5c6a716364e798c3c12537696fdb9
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2023-01-11 14:05:55 -0800

    Allow inference on Vector{i<:Dist}

--- src/dist/tuple.jl
+++ src/dist/tuple.jl
@@ -21,3 +21,12 @@ frombits(x::Matrix, world) =
     map(v -> frombits(v, world), x)
 Base.ifelse(cond::Dist{Bool}, then::Tuple, elze::Tuple) =
     Tuple(ifelse(cond, x, y) for (x, y) in zip(then,elze))
+
+tobits(x::Vector) = 
+    mapreduce(tobits, vcat, x)
+
+frombits(x::Vector, world) = 
+    map(v -> frombits(v, world), x)
+
+Base.ifelse(cond::Dist{Bool}, then::Vector, elze::Vector) =
+    Vector(ifelse(cond, x, y) for (x, y) in zip(then,elze))
