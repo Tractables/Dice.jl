b78dc4614842d6cacd38b2c9033f32096ea0e1aa
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2023-01-25 19:42:47 -0800

    Fix DistVector tests

--- test/dist/vector_test.jl
+++ test/dist/vector_test.jl
@@ -28,66 +28,13 @@ using Dice
     @test dist[[7, 6, 5, 333, 444]] ≈ 2/5 * 1/3 * 1/10
     @test dist[[7, 6, 5, 555]] ≈ 2/5 * 1/3 * 9/10
 
-
-    # Test concatenation for empty vectors
-    cg = @dice begin
-        prob_extend(DistVector{DistUInt32}(), DistVector{DistUInt32}())
-    end
-    dist = pr(cg)
-    @test sum(values(dist)) ≈ 1
-    @test dist[[]] ≈ 1
-
-
-    cg = @dice begin
-        prob_extend(DistVector{DistString}(Vector{DistString}()), DistVector([DistString("hi")]))
-    end
-    dist = pr(cg)
-    @test sum(values(dist)) ≈ 1
-    @test dist[["hi"]] ≈ 1
-
-
-    # Test getindex, setindex
-    cg = @dice begin
-        s = if flip(0.6)
-            DistVector(Vector{AnyBool}([flip(0), flip(0), flip(0)]))
-        else
-            DistVector(Vector{AnyBool}([flip(1), flip(1)]))
-        end
-
-        # Choose whether to change index 1 (Pr=0.3) or 2 (Pr = 0.7)
-        i = if flip(0.3) DistUInt32(1) else DistUInt32(2) end
-
-        c = if flip(0.1) flip(0) else flip(1) end
-        s = prob_setindex(s, i, c)
-        prob_equals(DistVector([flip(0), flip(1), flip(0)]), s)
-    end
-    dist = pr(cg)
-    @test dist[true] ≈ 0.6*0.7*0.9
-
-    # Test prob_startswith
-    cg = @dice begin
-        s = DistVector(AnyBool[flip(0), flip(0.3), flip(0)])
-        t = DistVector(AnyBool[flip(0), flip(1)])
-        prob_startswith(s, t)
-    end
-    @test pr(cg)[true] ≈ 0.3
-
-    cg = @dice begin
-        s = DistVector(AnyBool[flip(0)])
-        s = if flip(0.3) s else prob_append(s, flip(0.4)) end
-        t = DistVector(AnyBool[flip(0.9), flip(1)])
-        prob_startswith(s, t)
-    end
-    @test pr(cg)[true] ≈ 0.1 * 0.7 * 0.4
-
     cg = @dice begin
         v1 = DistVector{DistInt32}()
-        v2 = ifelse(flip(1/2), prob_append(v1, _6), v1)
-        v3 = ifelse(flip(1/2), prob_append(v2, _7), v2)
-        ifelse(flip(1/2), prob_append(v3, _6), v3)
+        v2 = ifelse(flip(1/2), prob_append(v1, DistInt32(6)), v1)
+        v3 = ifelse(flip(1/2), prob_append(v2, DistInt32(7)), v2)
+        ifelse(flip(1/2), prob_append(v3, DistInt32(6)), v3)
     end
     dist = pr(cg)
-    print(dist)
     @test length(dist) == 7
     @test dist[[]] ≈ 1/8
     @test dist[[7]] ≈ 1/8
@@ -96,10 +43,62 @@ using Dice
     @test dist[[6, 7]] ≈ 1/8
     @test dist[[7, 6]] ≈ 1/8
     @test dist[[6, 7, 6]] ≈ 1/8
-
+    
+    
+    # Test concatenation for empty vectors
+    cg = @dice begin
+        prob_extend(DistVector{DistUInt32}(), DistVector{DistUInt32}())
+    end
+    dist = pr(cg)
+    @test sum(values(dist)) ≈ 1
+    @test dist[[]] ≈ 1
+    
+    
+    cg = @dice begin
+        prob_extend(DistVector{DistString}(Vector{DistString}()), DistVector([DistString("hi")]))
+    end
+    dist = pr(cg)
+    @test sum(values(dist)) ≈ 1
+    @test dist[["hi"]] ≈ 1
+    
+    
+    # Test getindex, setindex
+    cg = @dice begin
+        s = if flip(0.6)
+            DistVector(Vector{AnyBool}([flip(0), flip(0), flip(0)]))
+        else
+            DistVector(Vector{AnyBool}([flip(1), flip(1)]))
+        end
+        
+        # Choose whether to change index 1 (Pr=0.3) or 2 (Pr = 0.7)
+        i = if flip(0.3) DistUInt32(1) else DistUInt32(2) end
+        
+        c = if flip(0.1) flip(0) else flip(1) end
+        s = prob_setindex(s, i, c)
+        prob_equals(DistVector([flip(0), flip(1), flip(0)]), s)
+    end
+    dist = pr(cg)
+    @test dist[true] ≈ 0.6*0.7*0.9
+    
+    # Test prob_startswith
+    cg = @dice begin
+        s = DistVector(AnyBool[flip(0), flip(0.3), flip(0)])
+        t = DistVector(AnyBool[flip(0), flip(1)])
+        prob_startswith(s, t)
+    end
+    @test pr(cg)[true] ≈ 0.3
+    
+    cg = @dice begin
+        s = DistVector(AnyBool[flip(0)])
+        s = if flip(0.3) s else prob_append(s, flip(0.4)) end
+        t = DistVector(AnyBool[flip(0.9), flip(1)])
+        prob_startswith(s, t)
+    end
+    @test pr(cg)[true] ≈ 0.1 * 0.7 * 0.4
+    
     # Test prob_contains
     _5, _6, _7, _8 = DistInt32(5), DistInt32(6), DistInt32(7), DistInt32(8)
-
+    
     cg = @dice begin
         s = DistVector([_5, _6, _7])
         if flip(0.3)
