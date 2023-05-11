b98ab448cbc219b8122e1d11af8de990d27416c5
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2023-05-11 12:11:57 -0700

    add option, restructure inductive type testst

---
+++ src/dist/inductive/option.jl
@@ -0,0 +1,13 @@
+
+export DistOpt, DistNone, DistSome
+
+struct DistOpt{T} <: DistInductiveType end
+function param_lists(::Type{DistOpt{T}})::Vector{Pair{String,Vector{Type}}} where T
+    [
+        "None" => [],
+        "Some" => [T],
+    ]
+end
+
+DistNone(T) = construct(DistOpt{T}, "None",  [])
+DistSome(x::T) where T = construct(DistOpt{T}, "Some", [x])
