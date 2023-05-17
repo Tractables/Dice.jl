59d7860306ee12b0a1f2bf8abc600e519c4a4880
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2023-05-16 17:31:36 -0700

    Remove update_group_to_psp!

--- src/flip_groups.jl
+++ src/flip_groups.jl
@@ -1,6 +1,6 @@
 # Flips whose probability is shared via global dicts
 
-export flip_for, get_group_probs, reset_flips!, update_group_to_psp!, get_group_prob
+export flip_for, get_group_probs, reset_flips!, get_group_prob
 
 # Map flip to group of flips that much share a probability
 _flip_to_group = Dict{Dice.Flip, Any}()
@@ -27,9 +27,4 @@ end
 function reset_flips!()
     global _flip_to_group = Dict{Dice.Flip, Any}()
     global _group_to_psp = Dict{Any, Float64}()
-end
-
-function update_group_to_psp!(group_to_psp::Dict{Any, Float64})
-    global _group_to_psp
-    _group_to_psp = group_to_psp
 end
