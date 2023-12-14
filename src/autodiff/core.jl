c0226ce2e79a9c02c32706665f783b9cd128a6d8
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2023-12-14 00:33:43 -0800

    [Autodiff] Arbitrary pr-dependent loss fns

--- src/autodiff/core.jl
+++ src/autodiff/core.jl
@@ -1,6 +1,4 @@
-export new_var!, clear_vars!, value, compute, differentiate, step_maximize!,
-    set_var!, sigmoid, add_unit_interval_var!, ADNode, Var, train_vars!, value,
-    ADMatrix, ad_map
+export value, compute, differentiate, sigmoid, ADNode, Var, value, ADMatrix, ad_map, Valuation, Derivs, compute_one, vars
 
 using DirectedAcyclicGraphs
 import DirectedAcyclicGraphs: NodeType, DAG, children
@@ -11,53 +9,13 @@ abstract type ADNode <: DAG end
 ADNodeCompatible = Union{Real, AbstractMatrix{<:Real}}
 
 struct Var <: ADNode
-    name::String
+    id::Any
 end
-
 NodeType(::Type{Var}) = Leaf()
-
-_variable_to_value = Dict{String, ADNodeCompatible}()
-
-function value(var::Var)
-    _variable_to_value[var.name]
-end
-
-var_exists(s) = haskey(_variable_to_value, s)
-
-function set_var!(s, val)
-    _variable_to_value[s] = val
-    Var(s)
-end
-
-function new_var!(s, init_val)
-    @assert !var_exists(s)
-    set_var!(s, init_val)
-end
-
-function get_or_init_var!(s, if_not_exists=nothing)
-    if var_exists(s)
-        Var(s)
-    else
-        set_var!(s, if_not_exists)
-    end
-end
 
 sigmoid(x) = 1 / (1 + exp(-x))
 deriv_sigmoid(x) = sigmoid(x) * (1 - sigmoid(x))
 inverse_sigmoid(x) = log(x / (1 - x))
-
-
-function add_unit_interval_var!(s, init_val=0.5)
-    @assert 0 < init_val < 1
-    before_sigmoid = get_or_init_var!(
-        "$(s)_before_sigmoid", inverse_sigmoid(init_val)
-    )
-    sigmoid(before_sigmoid)
-end
-
-function clear_vars!()
-    empty!(_variable_to_value)
-end
 
 struct Constant <: ADNode
     value::ADNodeCompatible
@@ -66,7 +24,7 @@ NodeType(::Type{Constant}) = Leaf()
 Base.zero(::ADNode) = Constant(0)
 
 Base.show(io::IO, x::Var) =
-    print(io, "Var($(x.name))")
+    print(io, "Var($(x.id))")
 Base.show(io::IO, x::Constant) =
     print(io, "Constant($(x.value))")
 
@@ -206,35 +164,43 @@ end
 NodeType(::Type{Transpose}) = Inner()
 children(x::Transpose) = x.x
 
-function compute(root, vals=nothing)
-    isnothing(vals) && (vals = Dict{ADNode, Any}())
+Valuation = Dict{Var, ADNodeCompatible}
+Derivs = Dict{ADNode, ADNodeCompatible}
 
-    fl(x::Var) = value(x)
-    fl(x::Constant) = x.value
-    fi(x::Add, call) = call(x.x) + call(x.y)
-    fi(x::Mul, call) = call(x.x) * call(x.y)
-    fi(x::Pow, call) = call(x.x) ^ call(x.y)
-    # fi(x::Div, call) = call(x.x) / call(x.y)
-    fi(x::Log, call) = log(call(x.x))
-    # fi(x::Exp, call) = exp(call(x.x))
-    fi(x::Sin, call) = sin(call(x.x))
-    fi(x::Cos, call) = cos(call(x.x))
-    fi(x::GetIndex, call) = call(x.x)[x.i]
-    fi(x::ADMatrix, call) = map(call, x.x)
-    fi(x::Map, call) = map(x.f, call(x.x))
-    fi(x::Transpose, call) = transpose(call(x.x))
+# TODO: move to op definitions
+compute_leaf(x::Var) = error("The value of $(x) should've been provided in `vals`!")
+compute_leaf(x::Constant) = x.value
+compute_inner(x::Add, call) = call(x.x) + call(x.y)
+compute_inner(x::Mul, call) = call(x.x) * call(x.y)
+compute_inner(x::Pow, call) = call(x.x) ^ call(x.y)
+# compute_inner(x::Div, call) = call(x.x) / call(x.y)
+compute_inner(x::Log, call) = log(call(x.x))
+# compute_inner(x::Exp, call) = exp(call(x.x))
+compute_inner(x::Sin, call) = sin(call(x.x))
+compute_inner(x::Cos, call) = cos(call(x.x))
+compute_inner(x::GetIndex, call) = call(x.x)[x.i]
+compute_inner(x::ADMatrix, call) = map(call, x.x)
+compute_inner(x::Map, call) = map(x.f, call(x.x))
+compute_inner(x::Transpose, call) = transpose(call(x.x))
 
-    foldup(root, fl, fi, ADNodeCompatible, vals)
+function compute_one(root, vals::Dict{ADNode, <:ADNodeCompatible})
+    foldup(root, compute_leaf, compute_inner, ADNodeCompatible, vals)
 end
 
-function differentiate(root_derivs::AbstractDict{<:ADNode, <:ADNodeCompatible})
+function compute(var_vals::Valuation, roots)
     vals = Dict{ADNode, ADNodeCompatible}()
-    for root in keys(root_derivs)
-        compute(root, vals)
+    merge!(vals, var_vals)
+    for root in roots
+        compute_one(root, vals)
     end
-    
+    vals
+end
+
+function differentiate(var_vals::Valuation, root_derivs::Derivs)
+    vals = compute(var_vals, keys(root_derivs))
     derivs = Dict{ADNode, ADNodeCompatible}()
     merge!(derivs, root_derivs)
+    # TODO: move these to operator definition sites
     f(::Constant) = nothing
     f(::Var) = nothing
     function add_deriv(n::ADNode, amount::ADNodeCompatible)
@@ -331,25 +297,6 @@ function foreach_down(f::Function, roots)
     end
 end
 
-function step_maximize!(roots, learning_rate)
-    root_derivs = Dict(
-        root => 1
-        for root in roots
-    )
-    derivs = differentiate(root_derivs)
-    for (n, d) in derivs
-        if n isa Var
-            _variable_to_value[n.name] += d * learning_rate
-        end
-    end
-end
-
-function train_vars!(
-    x::ADNode,
-    epochs::Integer=2000,
-    learning_rate::AbstractFloat=0.003,
-)
-    for _ in 1:epochs
-        step_maximize!([x], learning_rate)
-    end
+function vars(x::ADNode)
+    filter(node -> node isa Var, x)
 end
