d9daee6684182bc844b9d5e8b5f25e4ea7db005e
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2023-12-23 18:00:59 -0800

    fix darts example

--- examples/darts.jl
+++ examples/darts.jl
@@ -13,21 +13,24 @@ all(itr) = reduce(&, itr)
 any(itr) = reduce(|, itr)
 
 DARTS_PER_COLOR = [1, 2, 10] # number of red, green, and blue darts
-weights = [var!("r", 1), var!("g", 1), var!("b", 1)]
+weights = [Var("r"), Var("g"), Var("b")]
+var_vals = Valuation(weight => 1 for weight in weights)
 
 all_colors_receive_own_dart = all(
   any(flip(weight / sum(weights)) for _ in 1:num_own_darts)
   for (num_own_darts, weight) in zip(DARTS_PER_COLOR, weights)
 )
 
-pr(all_colors_receive_own_dart) # 0.182
-train_vars!([all_colors_receive_own_dart]; epochs=1000, learning_rate=0.3)
+pr_all_colors_receive_own_dart = exp(LogPr(all_colors_receive_own_dart))
+
+compute_mixed(var_vals, pr_all_colors_receive_own_dart) # 0.182
+train!(var_vals, -LogPr(all_colors_receive_own_dart); epochs=1000, learning_rate=0.3)
 
 # We've increased the chance of success!
-pr(all_colors_receive_own_dart) # 0.234
+compute_mixed(var_vals, pr_all_colors_receive_own_dart) # 0.234
 
 # Compute what ratio we actually need to paint the target:
-[compute(weight/sum(weights)) for weight in weights]
+[compute(var_vals, weight/sum(weights)) for weight in weights]
 # 3-element Vector{Float64}:
 #  0.46536681058883267
 #  0.3623861813855715
