1c0ff362b40349dcf020ec045d661910d9fe8849
PoorvaGarg <poorva98@gmail.com>
2023-12-26 14:44:17 -0800

    mini experiments added

--- figures/gamma.jl
+++
@@ -1,9 +0,0 @@
-using Dice
-using Plots
-
-code = @dice unit_gamma(DistFix{15, 14}, 1, -7.0)
-a = pr(code)
-plot(a)
-
-plot(a, line = (:solid), label=false, linewidth=5, xaxis = ("x"), yaxis = ("pr(x)"), xguidefontsize=30, xtickfontsize=15, yguidefontsize=30, ytickfontsize=15)
-savefig("fig1a"*string(14)*".pdf")
--- mini-experiments/plot_gauss/plot_gauss.jl
+++ mini-experiments/plot_gauss/plot_gauss.jl
@@ -1,15 +1,11 @@
 using Dice, Distributions
 using Plots
 
-
+# Plot approximations of gaussian
 function plot_gaussian(bits::Int, pieces::Int)
     DFiP = DistFix{4 + bits, bits}
-    code = @dice begin 
-                a = bitblast_exponential(DFiP, Normal(0, 1), pieces, -8.0, 8.0)
-                a
-    end
-    p = pr(code)
-    p
+    code = @dice bitblast_exponential(DFiP, Normal(0, 1), pieces, -8.0, 8.0)
+    pr(code)
 end
 
 function gen_plots(bit_vector)
@@ -26,25 +22,9 @@ function gen_plots(bit_vector)
         for i in 2:j+3
             plot!(plot_vector[i], line = (:solid), marker = (:circle), label = "p="*string(2^i))
         end
-        savefig("fig2_"*string(j)*".png")
+        savefig("mini-experiments/plot_gauss/fig_"*string(j)*".png")
     end
 end
 
 bits = [0, 1, 4, 5]
 gen_plots(bits)
-
-# for j in bit_vector
-    j = 7
-    plot_vector = Vector(undef, j+3)
-    label_string = Vector(undef, j+3)
-    for i in 7:7
-        p = 2^7
-        a = plot_gaussian(j, p)
-        plot_vector[i] = a
-        label_string[i] = "p="*string(128)
-    end
-    plot(plot_vector[7], line = (:solid), label=false, linewidth=5, xaxis = ("x"), yaxis = ("pr(x)"), xguidefontsize=30, xtickfontsize=15, yguidefontsize=30, ytickfontsize=15)
-    for i in 8:7
-        plot!(plot_vector[i], line = (:solid), label = "p="*string(2^i))
-    end
-    savefig("fig2_"*string(j)*".png")
