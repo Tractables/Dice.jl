238b2f7d86ebd2158becf5b4c737b32d00ace9a3
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2024-02-04 16:06:08 -0800

    add stop if inf or nan in train\!, old unif impl

--- src/dist/number/uint.jl
+++ src/dist/number/uint.jl
@@ -433,3 +433,35 @@ function unif_half(hi::DistUInt{W})::DistUInt{W} where W
     rem_trunc(u, hi)
 end
 
+# function unif_half(hi::DistUInt{W})::DistUInt{W} where W
+#     max_hi = maxvalue(hi)
+#     max_hi > 60 && error("Likely to time out")
+#     prod = BigInt(1)
+#     for prime in primes_at_most(max_hi)
+#         prod *= prime ^ floor_log(prime, max_hi)
+#     end
+#     u = uniform(DistUInt{ndigits(prod, base=2)}, 0, prod)
+#     rem_trunc(u, hi)
+# end
+
+function primes_at_most(n::Int)
+    isprime = [true for _ in 1:n]
+    for p in 2:trunc(Int, sqrt(n))
+        if isprime[p]
+            for i in p^2:p:n
+                isprime[i] = false
+            end
+        end
+    end
+    [i for i in 2:n if isprime[i]]
+end
+
+function floor_log(base, n)
+    v = 1
+    pow = 0
+    while v * base <= n
+        v *= base
+        pow += 1
+    end
+    pow
+end
