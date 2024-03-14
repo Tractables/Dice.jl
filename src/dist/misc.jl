f3c69e25c45527e286e54313db30f55f9830fb9f
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2024-03-13 21:42:41 -0700

    vec/tuple: add frombits_as_dist and prob_equals

--- src/dist/misc.jl
+++ src/dist/misc.jl
@@ -1,3 +1,5 @@
+prob_all(xs) = reduce(&, xs; init=true)
+
 ##################################
 # Nothing
 ##################################
@@ -15,8 +17,16 @@ tobits(x::Tuple) =
 frombits(x::Tuple, world) = 
     map(v -> frombits(v, world), x)
 
+frombits_as_dist(x::Tuple, world) =
+    map(v -> frombits_as_dist(v, world), x)
+
 Base.ifelse(cond::Dist{Bool}, then::Tuple, elze::Tuple) =
     Tuple(ifelse(cond, x, y) for (x, y) in zip(then,elze))
+
+function prob_equals(x::Tuple, y::Tuple)
+    @assert length(x) == length(y)
+    prob_all(prob_equals(a, b) for (a, b) in zip(x, y))
+end
 
 tobits(x::Vector) = 
     mapreduce(tobits, vcat, x)
@@ -24,7 +34,15 @@ tobits(x::Vector) =
 frombits(x::Vector, world) = 
     map(v -> frombits(v, world), x)
 
+frombits_as_dist(x::Vector, world) =
+    map(v -> frombits_as_dist(v, world), x)
+
 Base.ifelse(cond::Dist{Bool}, then::Vector, elze::Vector) =
     [ifelse(cond, x, y) for (x, y) in zip(then,elze)]
+
+function prob_equals(x::Vector, y::Vector)
+    @assert length(x) == length(y)
+    prob_all(prob_equals(a, b) for (a, b) in zip(x, y))
+end
 
 
