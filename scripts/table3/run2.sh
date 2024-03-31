6349586e3dbaf50d46828e2ce004d18737aed121
PoorvaGarg <poorva98@gmail.com>
2024-03-30 20:18:19 -0700

    Revert "psi experiments updated"
    This reverts commit 9a6c0697b23e75137ec5b00a9e6330e9d0ad81c4.

--- scripts/table3/run2.sh
+++
@@ -1,9 +0,0 @@
-for i in baselines/psi/*.psi;do
-    echo "Running $i"
-    touch $i.m
-    echo "N[" > $i.m
-    timeout 1200s /space/poorvagarg/PLDI2023/psi/psi --expectation --raw --mathematica $i >> $i.m
-    echo ",10]" >> $i.m
-    echo $i-results.txt
-    timeout 1200s /space/poorvagarg/Mathematica14/Executables/math < $i.m > $i-results.txt
-done
