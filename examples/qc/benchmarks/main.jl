261daf655740fb020f204e6eee2c8264e095d101
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2024-03-31 19:19:55 -0700

    tweaks

--- examples/qc/benchmarks/main.jl
+++ examples/qc/benchmarks/main.jl
@@ -1,34 +1,36 @@
 include("benchmarks.jl")
 
 
-# GENERATION_PARAMS_LIST = [
-#     TypeBasedRBTGenerator(size=5,color_by_size=true,learn_leaf_weights=false,use_parent_color=true),
-#     TypeBasedRBTGenerator(size=5,color_by_size=true,learn_leaf_weights=false,use_parent_color=false),
-# ]
-# LR_LIST = [0.1, 0.3, 1, 3, 10, 30, 100, 300]
-# LOSS_CONFIG_WEIGHT_PAIRS_LIST = collect(Iterators.flatten([
-#     (
-#         [
-#             SamplingEntropy{RBT}(resampling_frequency=2, samples_per_batch=10000) => lr,
-#         ]
-#         for lr in LR_LIST
-#     ),
-# ]))
-# EPOCHS_LIST = [100_000]
-
-GENERATION_PARAMS_LIST = [Flips{3}()]
-# LR_LIST = [0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30, 100, 300]
-LR_LIST = [0.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 3, 10, 30, 100]
+GENERATION_PARAMS_LIST = [
+    BespokeSTLCGenerator(param_vars_by_size=true,size=5,ty_size=2),
+    # BespokeBSTGenerator(size=5, vals=BSTDummyVals),
+    # TypeBasedBSTGenerator(size=5),
+]
+LR_LIST = [0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10]
 LOSS_CONFIG_WEIGHT_PAIRS_LIST = collect(Iterators.flatten([
     (
         [
-            # BoolsExactEntropy{3}() => lr,
-            SamplingEntropy{Bools{3}}(resampling_frequency=20, samples_per_batch=100) => lr,
+            SamplingEntropy{STLC}(resampling_frequency=2, samples_per_batch=1_000) => lr,
         ]
         for lr in LR_LIST
     ),
 ]))
 EPOCHS_LIST = [10_000]
+
+# N = 4
+# GENERATION_PARAMS_LIST = [Flips{N}()]
+# # LR_LIST = [0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30, 100, 300]
+# LR_LIST = [0.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 3, 10, 30, 100]
+# LOSS_CONFIG_WEIGHT_PAIRS_LIST = collect(Iterators.flatten([
+#     (
+#         [
+#             # BoolsExactEntropy{3}() => lr,
+#             SamplingEntropy{Bools{N}}(resampling_frequency=10, samples_per_batch=100) => lr,
+#         ]
+#         for lr in LR_LIST
+#     ),
+# ]))
+# EPOCHS_LIST = [10_000]
 
 TOOL_PATH = "examples/qc/benchmarks/tool.jl"
 
