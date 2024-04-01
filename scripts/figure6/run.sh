70d32809c83b296475530e25a883d12834d8760f
PoorvaGarg <poorva98@gmail.com>
2024-04-01 09:25:50 -0700

    fixed file not found error

--- scripts/figure6/run.sh
+++ scripts/figure6/run.sh
@@ -3,4 +3,4 @@ webppl benchmarks/conjugate_gaussians/conjugate_gaussians.wppl -- --s $((2**20))
 webppl benchmarks/conjugate_gaussians/conjugate_gaussians.wppl -- --s $((2**16)) --m SMC >> benchmarks/conjugate_gaussians/result_new1SMC.txt
 
 cd ../AQUA
-java -cp "target/aqua-1.0.jar:lib/storm-1.0.jar" aqua.analyses.AnalysisRunner benchmarks/conjugate_gaussians/conjugate_gaussians_aqua 40401
+java -cp "target/aqua-1.0.jar:lib/storm-1.0.jar" aqua.analyses.AnalysisRunner ../Dice.jl/benchmarks/conjugate_gaussians/conjugate_gaussians_aqua 40401
