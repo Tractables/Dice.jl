7aa86fa3d730cc88ed751f14a640069727dc600a
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2024-04-15 22:22:10 -0700

    update output

---
+++ examples/qc/benchmarks/output/qc15_cond_ent_better_loss/rbt/typebased/sz=5/color_by_size=true/learn_leaf_weights=false/use_parent_color=false/reinforce_sampling_entropy/freq=2-spb=100/0.01/bookkeepingANDbalance/0.15/epochs=20000/trained_Generator.v
@@ -0,0 +1,146 @@
+Require Import ZArith.
+From QuickChick Require Import QuickChick.
+From ExtLib Require Import Monad.
+From ExtLib.Data.Monads Require Import OptionMonad.
+Import QcNotation.
+Import MonadNotation.
+From Coq Require Import List.
+Import ListNotations.
+
+From RBT Require Import Impl Spec.
+
+(* Look up in list of backtrack weights *)
+Fixpoint get {a: Type} (l : list (nat * a)) (target_key : nat) (default : a): a :=
+  match l with
+  | [] =>
+    (* This branch should never return *)
+    default
+  | (key, value) :: l' =>
+    if Nat.eqb key target_key then
+       value
+    else get l' target_key default
+  end.
+
+Definition manual_gen_tree :=
+    fun s : nat =>
+    (let
+       fix arb_aux (size : nat) (parent_color : Color) : G Tree :=
+         let weight_red := match parent_color with 
+         | R =>
+          get [
+            (1, 500); (2, 506); (3, 488); (4, 997)
+          ] s 0
+         | B =>
+          get [
+            (1, 500); (2, 506); (3, 488); (4, 997); (5, 4)
+          ] s 0
+         end in
+         let weight_leaf := 500 in
+         match size with
+         | 0 => returnGen E
+         | S size' =>
+             freq [ (weight_leaf, returnGen E);
+             (1000 - weight_leaf,
+             bindGen (freq [ (weight_red, returnGen R); (1000-weight_red, returnGen B)])
+               (fun p0 : Color =>
+                bindGen (arb_aux size' p0)
+                  (fun p1 : Tree =>
+                   bindGen arbitrary
+                     (fun p2 : Z =>
+                      bindGen arbitrary
+                        (fun p3 : Z =>
+                         bindGen (arb_aux size' p0)
+                           (fun p4 : Tree => returnGen (T p0 p1 p2 p3 p4)))))))]
+         end in
+     arb_aux) s.
+
+#[global]
+Instance genTree : GenSized (Tree) := 
+  {| arbitrarySized n := manual_gen_tree n B |}.
+
+(* --------------------- Tests --------------------- *)
+
+Definition test_prop_InsertValid :=  
+    forAll arbitrary (fun t =>    
+    forAll arbitrary (fun k =>
+    forAll arbitrary (fun v =>
+        (prop_InsertValid t k v)))).
+
+(*! QuickChick test_prop_InsertValid. *)
+
+Definition test_prop_DeleteValid :=  
+    forAll arbitrary (fun t =>    
+    forAll arbitrary (fun k =>
+        prop_DeleteValid t k)).
+
+(*! QuickChick test_prop_DeleteValid. *)
+
+Definition test_prop_InsertPost :=  
+    forAll arbitrary (fun t =>    
+    forAll arbitrary (fun k =>
+    forAll arbitrary (fun k' =>
+     forAll arbitrary (fun v =>
+        prop_InsertPost t k k' v)))).
+
+(*! QuickChick test_prop_InsertPost. *)
+
+Definition test_prop_DeletePost := 
+    forAll arbitrary (fun t =>    
+    forAll arbitrary (fun k =>
+    forAll arbitrary (fun k' =>
+        prop_DeletePost t k k'))).
+
+(*! QuickChick test_prop_DeletePost. *)
+    
+Definition test_prop_InsertModel :=  
+    forAll arbitrary (fun t =>    
+    forAll arbitrary (fun k =>
+    forAll arbitrary (fun v =>
+        prop_InsertModel t k v))).
+
+(*! QuickChick test_prop_InsertModel. *)
+    
+Definition test_prop_DeleteModel :=  
+    forAll arbitrary (fun t =>    
+    forAll arbitrary (fun k =>
+            prop_DeleteModel t k)).
+
+(*! QuickChick test_prop_DeleteModel. *)
+
+Definition test_prop_InsertInsert :=  
+    forAll arbitrary (fun t =>    
+    forAll arbitrary (fun k =>
+    forAll arbitrary (fun k' =>
+    forAll arbitrary (fun v =>
+    forAll arbitrary (fun v' =>     
+        prop_InsertInsert t k k' v v'))))).
+
+(*! QuickChick test_prop_InsertInsert. *)
+    
+Definition test_prop_InsertDelete := 
+    forAll arbitrary (fun t =>    
+    forAll arbitrary (fun k =>
+    forAll arbitrary (fun k' =>
+    forAll arbitrary (fun v =>
+        prop_InsertDelete t k k' v)))).
+
+(*! QuickChick test_prop_InsertDelete. *)
+    
+Definition test_prop_DeleteInsert := 
+    forAll arbitrary (fun t =>    
+    forAll arbitrary (fun k =>
+    forAll arbitrary (fun k' =>
+    forAll arbitrary (fun v' =>
+        prop_DeleteInsert t k k' v')))).
+
+(*! QuickChick test_prop_DeleteInsert. *)
+    
+Definition test_prop_DeleteDelete :=  
+    forAll arbitrary (fun t =>    
+    forAll arbitrary (fun k =>
+    forAll arbitrary (fun k' =>
+        prop_DeleteDelete t k k'))).
+
+(*! QuickChick test_prop_DeleteDelete. *)
+
+
---
+++ examples/qc/benchmarks/output/v13/stlc/bespoke/by_sz/sz=5-tysz=2/reinforce_sampling_entropy/freq=2-spb=1000/0.03/epochs=10000/initial_Generator.v
@@ -0,0 +1,160 @@
+From QuickChick Require Import QuickChick. Import QcNotation.
+From Coq Require Import Bool ZArith List. Import ListNotations.
+From ExtLib Require Import Monad.
+From ExtLib.Data.Monads Require Import OptionMonad.
+Import MonadNotation.
+
+From STLC Require Import Impl Spec.
+
+Derive (Arbitrary) for Typ.
+Derive (Arbitrary) for Expr.
+
+Inductive bind : Ctx -> nat -> Typ -> Prop :=
+| BindNow   : forall tau env, bind (tau :: env) 0 tau
+| BindLater : forall tau tau' x env,
+    bind env x tau -> bind (tau' :: env) (S x) tau.
+
+Inductive typing (G : Ctx) : Expr -> Typ -> Prop :=
+| TyVar :
+    forall x T,
+      bind G x T ->
+      typing G (Var x) T
+| TyBool :
+    forall b,
+      typing G (Bool b) TBool
+| TyAbs :
+    forall e T1 T2,
+      typing (T1 :: G) e T2 ->
+      typing G (Abs T1 e) (TFun T1 T2)
+| TyApp :
+    forall e1 e2 T1 T2,
+      typing G e1 (TFun T1 T2) ->
+      typing G e2 T1 ->
+      typing G (App e1 e2) T2.
+
+
+(* Look up in list of backtrack weights *)
+Fixpoint get {a: Type} (l : list (nat * a)) (target_key : nat) (default : a): a :=
+  match l with
+  | [] =>
+    (* This branch should never return *)
+    default
+  | (key, value) :: l' =>
+    if Nat.eqb key target_key then
+       value
+    else get l' target_key default
+  end.
+
+
+#[export] Instance dec_type (t1 t2 : Typ) : Dec (t1 = t2).
+Proof. dec_eq. Defined.
+Derive Arbitrary for Typ.
+
+Fixpoint genVar' (ctx: Ctx) (t: Typ) (p: nat) (r: list nat) : list nat :=
+  match ctx with
+  | nil => r
+  | t'::ctx' =>
+      if t = t'? then genVar' ctx' t (p + 1) (p :: r)
+      else genVar' ctx' t (p + 1) r
+  end.
+
+Fixpoint genZero env tau : G (option Expr) :=
+  match tau with
+  | TBool =>
+      bindGen arbitrary
+              (fun b : bool =>
+                 returnGen (Some (Bool b)))
+  | TFun T1 T2 => 
+      bindOpt
+        (genZero (T1 :: env) T2)
+        (fun e : Expr =>
+           returnGen (Some (Abs T1 e)))
+  end.
+
+Fixpoint genTyp (s: nat) : G Typ :=
+  match s with
+  | 0 => ret TBool
+  | S s' =>
+    let '(boolWeight, funWeight) :=
+      get
+      [
+        (1, (500, 1000-500));
+        (2, (500, 1000-500))
+      ]
+      s
+      (0, 0) in
+    freq [
+      (boolWeight, ret (TBool))
+      ;
+      (funWeight,
+          t1 <- genTyp s';;
+          t2 <- genTyp s';;
+          ret (TFun t1 t2))
+    ]
+  end.
+
+Fixpoint genExpr env tau (sz: nat) : G (option Expr) :=
+  match sz with
+  | 0 =>
+    let '(var_weight, zero_weight) := (500, 1000-500) in
+      backtrack
+        [(var_weight, oneOf_ (ret None) (map (fun x => returnGen (Some (Var x))) (genVar' env tau 0 [])))
+        ;(zero_weight, genZero env tau)] 
+  | S sz' =>
+      let '(val_weight, app_weight, var_weight) :=
+        get
+          [
+            (1, (500, 500, 500));
+            (2, (500, 500, 500));
+            (3, (500, 500, 500));
+            (4, (500, 500, 500));
+            (5, (500, 500, 500))
+          ]
+          sz
+          (0, 0 ,0) in
+      backtrack
+        [(var_weight, oneOf_ (ret None) (map (fun x => returnGen (Some (Var x))) (genVar' env tau 0 [])))
+         ;
+        (app_weight,
+          bindGen (genTyp 2)
+                  (fun T1 : Typ =>
+          bindOpt
+            (genExpr env (TFun T1 tau) sz')
+            (fun e1 : Expr =>
+          bindOpt
+            (genExpr env T1 sz')
+            (fun e2 : Expr =>
+               returnGen (Some (App e1 e2))))))
+         ; 
+         (val_weight,
+           match tau with
+           | TBool =>
+               bindGen arbitrary
+                       (fun b : bool =>
+                          returnGen (Some (Bool b)))
+           | TFun T1 T2 =>
+               bindOpt
+                 (genExpr (T1 :: env) T2 sz')
+                    (fun e : Expr =>
+                     returnGen (Some (Abs T1 e)))
+              end)]
+  end.
+
+Definition gSized := 
+    typ <- arbitrary ;;
+    genExpr [] typ 5.
+
+
+Definition test_prop_SinglePreserve :=
+  forAllMaybe gSized (fun (e: Expr) =>
+    prop_SinglePreserve e).
+
+(*! QuickChick test_prop_SinglePreserve. *)
+  
+Definition test_prop_MultiPreserve :=
+  forAllMaybe gSized (fun (e: Expr) =>
+    prop_MultiPreserve e).
+  
+(*! QuickChick test_prop_MultiPreserve. *)
+
+
