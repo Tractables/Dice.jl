585a125ef00a397e9af6dd690ef74024d636977a
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2024-05-01 12:16:51 -0700

    add Opt.bind, Opt.map

--- src/dist/inductive/option.jl
+++ src/dist/inductive/option.jl
@@ -4,4 +4,20 @@ export Opt
 module Opt
     using Dice
     @inductive T{A} None() Some(A)
+
+    Some(x) = Some(typeof(x), x)
+
+    function bind(f, T, x::Opt.T)
+        @match x [
+            None() -> None(T),
+            Some(x) -> f(x)
+        ]
+    end
+
+    function map(f, T, x::Opt.T)
+        @match x [
+            None() -> None(T),
+            Some(x) -> Some(T, f(x))
+        ]
+    end
 end
