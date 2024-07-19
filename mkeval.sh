38da216c1de8cfe63ce17d04aadc5ed4d1c4a844
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2024-07-18 19:58:41 -0700

    remove mkeval.sh

--- mkeval.sh
+++
@@ -1,6 +0,0 @@
-TO=/space2/tjoa/tuning-output
-cat "$(dirname $(find $TO -name *BSmallTrainedGenerator*))/log.log" | head -n 1
-cat "$(dirname $(find $TO -name *RLSDThinSmallGenerator*))/log.log" | head -n 1
-cat "$(dirname $(find $TO -name *SLSDThinEqWellLR30Bound10Generator*))/log.log" | head -n 1
-cat "$(dirname $(find $TO -name *SBespokeACELR03Bound10Generator*))/log.log" | head -n 1
-
