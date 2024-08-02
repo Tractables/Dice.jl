d0c57d5dfdc7f534e104ac5ec79654f31f3ab9c3
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2024-08-02 15:04:08 -0700

    no flips

--- examples/qc/benchmarks/main.jl
+++ examples/qc/benchmarks/main.jl
@@ -144,29 +144,29 @@ LOSS_CONFIG_WEIGHT_PAIRS_LIST = collect(Iterators.flatten([
 ]))
 
 
-N = 3
-GENERATION_PARAMS_LIST = [Flips{N}()]
+# N = 3
+# GENERATION_PARAMS_LIST = [Flips{N}()]
 # LR_LIST = [0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30, 100, 300]
-LR_LIST = [0.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 3, 10, 30, 100]
-LOSS_CONFIG_WEIGHT_PAIRS_LIST = collect(Iterators.flatten([
-    (
-        [
-            # BoolsExactEntropy{3}() => lr,
-            SamplingEntropy{Bools{N}}(
-                resampling_frequency=1,
-                samples_per_batch=300,
-                property=TrueProperty{Bools{N}}(),
-                eq=:prob_equals,
-                failure_penalty=0.,
-                forgiveness=0,
-                rand_forgiveness=true,
-                keyf=:identity,
-            ) => lr,
-        ]
-        for lr in LR_LIST
-    ),
-]))
-EPOCHS_LIST = [100]
+# LR_LIST = [0.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 3, 10, 30, 100]
+# LOSS_CONFIG_WEIGHT_PAIRS_LIST = collect(Iterators.flatten([
+#     (
+#         [
+#             # BoolsExactEntropy{3}() => lr,
+#             SamplingEntropy{Bools{N}}(
+#                 resampling_frequency=1,
+#                 samples_per_batch=300,
+#                 property=TrueProperty{Bools{N}}(),
+#                 eq=:prob_equals,
+#                 failure_penalty=0.,
+#                 forgiveness=0,
+#                 rand_forgiveness=true,
+#                 keyf=:identity,
+#             ) => lr,
+#         ]
+#         for lr in LR_LIST
+#     ),
+# ]))
+# EPOCHS_LIST = [100]
 
 TOOL_PATH = "examples/qc/benchmarks/tool.jl"
 
