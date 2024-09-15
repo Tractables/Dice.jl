0e7306b5b71022b7997539259e13f5da0ee11278
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2024-09-15 16:17:35 -0400

    mini qc examples

--- qc/examples/adev_testing.jl
+++ qc/examples/adev_testing.jl
@@ -1,144 +1,50 @@
-# Learning flip probs to change the length distribution of a list generator.
+using Revise
 using Dice
 using ADEV
 using Infiltrator
+using ForwardDiff
+using Random
+using StatsBase: mean, std
+using ReverseDiff
 
-# Define inductive datatype for nat lists
-@inductive DistNatList DistNil() DistCons(DistUInt32, DistNatList)
-
-function length(l::DistNatList)
-    @match l [
-        DistNil() -> DistUInt32(0),
-        DistCons(x, xs) -> DistUInt32(1) + length(xs),
-    ]
-end
-
-# Our generator for nat lists
-function gen_list(size)
-    size == 0 && return DistNil()
-
-    # After understanding this demo as-written, try replacing `size` in the next
-    # line with a constant, which would force all sizes to use the same
-    # probability. This would restrict the distribution over list lengths to
-    # roughly geometric distributions, so the final distribution wouldn't be as
-    # close to the desired (but still better than before training).
-    @dice_ite if flip(sigmoid(Var(size)))
-        DistNil()
-    else
-        # The flips used in `uniform` use concrete weights, so we don't learn
-        # their probabilities (but we could!).
-        DistCons(uniform(DistUInt32, 0, 10), gen_list(size-1))
-    end
-end
-
-# Top-level size/fuel. For gen_list, this is the max length.
-INIT_SIZE = 5
-
-# Build computation graph of Dists. We could inline these definitions at their
-# use sites (constructing the computation graph is pure), but this saves a tiny
-# bit of work.
-list = gen_list(INIT_SIZE)
-list_len = length(list)
-
-var_vals = Valuation(Var(size) => 0 for size in 1:INIT_SIZE)
-
-println("Distribution before training:")
-display(pr_mixed(var_vals)(list_len))
-println()
-
-# Approximate a distribution using KL divergence
-desired_dist = [
-    DistUInt32(0) => 0.3,
-    DistUInt32(2) => 0.2,
-    DistUInt32(5) => 0.5
-]
-loss = kl_divergence(desired_dist, list_len)
-
-@infiltrate
-
-
-function f(theta)
-    @adev begin
-        let x = @sample(flip_mvd(theta[1])) & @sample(flip_mvd(theta[1])) & !@sample(flip_mvd(theta[1])),
-            y = @sample(flip_mvd(theta[2])) & !@sample(flip_mvd(theta[2])) & !@sample(flip_mvd(theta[2]))
-            x & y
+function mk_adev_prog(fl)
+    function (theta)
+        @adev begin
+            # let x = @sample(fl(theta[1])) & @sample(fl(theta[1])) & !@sample(fl(theta[1])),
+            #     y = @sample(fl(theta[2])) & !@sample(fl(theta[2])) & !@sample(fl(theta[2]))
+            #     x & y
+            # end
+            @sample(fl(theta[1])) & @sample(fl(theta[1])) & !@sample(fl(theta[1])) & @sample(fl(theta[2])) & !@sample(fl(theta[2])) & !@sample(fl(theta[2]))
         end
     end
 end
 
-fl = flip_reinforce
-function fab(a, b)
-    @adev begin
-        let x = @sample(fl(a)) & @sample(fl(a)) & !@sample(fl(a)),
-            y = @sample(fl(b)) & !@sample(fl(b)) & !@sample(fl(b))
-            x & y
-        end
-    end
-end
-gradient(fab; a=0.5, b=0.5)
+adev_prog = mk_adev_prog(flip_reinforce)
+dual_val = [ ForwardDiff.Dual(v, 1) for v in val ]
+N = 100
+approximate(f, N) = sum([f() for _ in 1:N]) / N
 
-ADEV.gradient(fab, 0.5, 0.5)
-
-
-ct = 0
-for seed in 1:1000
-    global ct
-    res = ReverseDiff.gradient( (args...) ->
-            fab([arg[1] for arg in args]...)(r -> (wants_grad, rng) -> r)(true, Random.Xoshiro(seed)),
-        ([0.5], [0.5])
-    )
-    if res != ([0.0], [0.0])
-        @show seed
-        display(res)
-        ct += 1
-    end
-end
-println(ct)
-
-
-ReverseDiff.gradient(
-    (args...) ->
-        fab([arg[1] for arg in args]...)(r -> (wants_grad, rng) -> r)(true, Random.Xoshiro(3)),
-    ([0.5], [0.5])
+reverse_mode_grad() = ReverseDiff.gradient(
+    (val) -> adev_prog(val)(r -> (wants_grad, rng) -> r)(true, Random.Xoshiro()),
+    val
+)
+forward_mode_grad() = ForwardDiff.gradient(
+    (val) -> adev_prog(val)(r -> (wants_grad, rng) -> r)(true, Random.Xoshiro()),
+    val
 )
 
 
-sgd(f; a=0.5, b=0.5, step_size=0.01, n_iters=1000, N=100)
+val = [0.5, 0.5]
+@time for _ in 1:1000
+    val += 0.01 * approximate(forward_mode_grad, N)
+end
+val
 
+val = [0.5, 0.5]
+@time for _ in 1:1000
+    val += 0.01 * approximate(reverse_mode_grad, N)
+end
+val
 
-train!(var_vals, loss, epochs=1000, learning_rate=0.3)
-
-# Done!
-println("Learned flip probability for each size:")
-display(Dict(size => compute(var_vals, sigmoid(Var(size))) for size in 1:INIT_SIZE))
-println()
-
-println("Distribution over lengths after training:")
-display(pr_mixed(var_vals)(list_len))
-
-#==
-Distribution before training:
-   0 => 0.5
-   1 => 0.25
-   2 => 0.12500000000000003
-   3 => 0.0625
-   4 => 0.03125
-   5 => 0.03125
-
-Learned flip probability for each size:
-Dict{Int64, Float64} with 5 entries:
-  5 => 0.3
-  4 => 0.00482717
-  2 => 0.00678713
-  3 => 0.285714
-  1 => 0.00678713
-
-Distribution over lengths after training:
-DataStructures.DefaultOrderedDict{Any, Any, Float64} with 6 entries:
-  5 => 0.490855
-  0 => 0.3
-  2 => 0.199035
-  1 => 0.00337902
-  3 => 0.00337718
-  4 => 0.00335426
-==#
+# why does this only give a single dual?
+adev_prog(dual_val)(r -> (wants_grad, rng) -> r)(true, Random.Xoshiro())
