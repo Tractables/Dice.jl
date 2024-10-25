c647786c343cc8f84acbfe83662ad9e75359bc69
PoorvaGarg <poorva98@gmail.com>
2024-10-25 16:57:41 -0700

    normal

--- examples/sample/normal.jl
+++ examples/sample/normal.jl
@@ -5,11 +5,11 @@ using Distributions
 
 # with observe
 bits = 2
-pieces = 2^(bits + 4)
-DFiP = DistFix{6 + bits, bits}
+pieces = 2^(bits + 1)
+DFiP = DistFix{4 + bits, bits}
 code = @dice begin
     w1 = bitblast(DFiP, Normal(0, sqrt(2)), pieces, -8.0, 8.0)
-    observe(prob_equals(w1.mantissa.number.bits[5+bits:6+bits], [true, false]))
+    observe(prob_equals(w1.mantissa.number.bits[3+bits:4+bits], [true, false]))
     w1
 end
 p1 = pr(code)
@@ -19,11 +19,11 @@ plot(p1)
 # without observe
 bits = 0
 pieces = 2^(bits + 3)
-DFiP = DistFix{6 + bits, bits}
+DFiP = DistFix{4 + bits, bits}
 code = @dice begin
     w1 = bitblast_sample(DFiP, Normal(0, sqrt(2)), pieces, -8.0, 8.0, 0.5, 0.25)
     # observe(prob_equals(w1.mantissa.number.bits[5+bits:6+bits], [true, true]))
-    DistFix{8, 2}([w1.mantissa.number.bits..., true, false])
+    DistFix{6, 2}([w1.mantissa.number.bits..., true, false])
 end
 p2 = pr(code)
 plot!(p2)
