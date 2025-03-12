769db14dcdd577ec322bec96bc49a6ffadadbe7a
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2025-03-12 16:31:35 -0700

    bound

--- stats/fewer_samples.sh
+++ stats/fewer_samples.sh
@@ -1,2 +1,5 @@
-# awk 'NR==1 {print; next} NR<=401 || (NR-1)%200==0' $1
-awk 'NR<=400 || NR%200==0' $1
+# HEADER:
+awk 'NR==1 {print; next} NR<=401 || (NR-1)%200==0' $1
+
+# NO HEADER:
+# awk 'NR<=400 || NR%200==0' $1
